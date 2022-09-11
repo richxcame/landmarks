4a125c66846cc1b6e1ddf40f9bfd3c3e8be8cac9
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-09-12 03:53:54 +0500

    fix: commit staged file

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Baygeldi Cholukov on 12.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
