0108b10dd4767a64f6d1e155805565e3ffe11128
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-09-12 01:33:38 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Baygeldi Cholukov on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
